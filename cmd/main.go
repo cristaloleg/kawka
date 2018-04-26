7b69206b0d8b90da5fdbe7c538c6f2f1514459ce
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-04-26 21:24:29 +0200

    better cmd

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,11 @@ import (
 )
 
 var (
-	addr    = flag.String("addr", ":8080", "The address to bind to")
-	brokers = flag.String("brokers", os.Getenv("KAFKA_PEERS"), "The Kafka brokers to connect to, as a comma separated list")
-	verbose = flag.Bool("verbose", false, "Turn on Sarama logging")
+	addr      = flag.String("addr", ":8080", "The address to bind to")
+	brokers   = flag.String("brokers", os.Getenv("KAFKA_PEERS"), "The Kafka brokers to connect to, as a comma separated list")
+	verbose   = flag.Bool("verbose", false, "Turn on Sarama logging")
+	topic     = flag.String("topic", "test", "topic name")
+	partition = flag.Int64("partition", 0, "partition")
 	// certFile  = flag.String("certificate", "", "The optional certificate file for client authentication")
 	// keyFile   = flag.String("key", "", "The optional key file for client authentication")
 	// caFile    = flag.String("ca", "", "The optional certificate authority file for TLS client authentication")
@@ -29,13 +31,16 @@ func init() {
 
 func main() {
 	brokerList := strings.Split(*brokers, ",")
+	if len(brokerList) == 0 {
+		panic("brokers are unavailable")
+	}
+
 	kawka := kawka.New(
 		kawka.WithBrokers(brokerList),
-		kawka.WithHandler(myHandler),
+		kawka.WithPort(5986),
+		kawka.WithHandler(func(data []byte) (string, []byte, error) {
+			return *topic, data, nil
+		}),
 	)
 	_ = kawka.Start()
-}
-
-func myHandler(data []byte) (topic string, content interface{}, err error) {
-	return "", nil, nil
 }
